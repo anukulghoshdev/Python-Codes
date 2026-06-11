a5d1d71efa9b0f4ef2b1cd2b1c212150817d11e1
Anukul Ghosh <anukulghosh264@gmail.com>
2026-06-11 19:11:47 +0600

    loops problem

--- Python-for-Ai/loops.py
+++ Python-for-Ai/loops.py
@@ -6,16 +6,70 @@ while i<=10:
         break
     i += 1
 i = 
+""" 
+
+
 """
-for i in range(0, 6, 2):
-    print("Hello world")
-
-
 i=0
 while i<10:
     i += 1
     if i == 5:
         continue
-    print(i)
+    # print(i)
+"""
 
+# for loops
+
+"""
+a = "Anukul ghosh"
+for i in a:
+    print(a[i])
+"""
+
+"""
+for i in range(1, 8):
+    if i == 4:
+        continue
+    else:
+        print(i)
+"""
+        
+"""
+n = int(input("Please Enter your number:- "))
+sum = 0
+for i in range(1, n+1):
+    sum = sum + i 
+print(f"your result is {sum}")
+"""
+
+"""
+n = int(input("Please Enter your factorial number:- "))
+fact = 1
+for i in range(1, n+1):
+    fact = fact * i 
+print(f"your factorial is {fact}")
+"""
+
+"""
+n = int(input("Enter your number:- "))
+even = 0
+odd = 0 
+for i in range(1, n+1):
+    if i % 2 == 0:
+        even = even + i
+    else:
+        odd = odd + i
+
+print(f"Your sum of even is {even} sum of odd is {odd}")
+"""
+
+"""
+count = 0
+n = int(input("Enter your number:- "))
+for i in range(1, n+1):
+    if n % i == 0:
+        count+=1
+
+print(f"the number of factors of {n} is:-  {count} ")
+""" 
 
